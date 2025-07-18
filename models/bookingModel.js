8f6e43bbd674e7848da8ca236de4f6cf9aba2274
tej-27 <nelloretejashwini@gmail.com>
2025-07-18 18:32:45 +0530

    Delete models directory

--- models/bookingModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const bookingSchema = new mongoose.Schema({
-  propertyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  userName: String,
-  status: { type: String, default: 'pending' }
-});
-module.exports = mongoose.model('Booking', bookingSchema);
--- models/userModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  type: { type: String, enum: ['Renter', 'Owner', 'Admin'] }
-});
-module.exports = mongoose.model('User', userSchema);
