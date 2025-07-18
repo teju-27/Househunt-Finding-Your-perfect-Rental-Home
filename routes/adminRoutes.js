28feb1e90181eafd4ef22d99c7e0a8abce2dfb82
tej-27 <nelloretejashwini@gmail.com>
2025-07-18 18:33:00 +0530

    Delete routes directory

--- routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllUsers, approveOwner } = require('../controllers/adminController');
-
-router.get('/users', getAllUsers);
-router.patch('/approve-owner/:id', approveOwner);
-
-module.exports = router;
